664b5051696610d71bb6431228e088975baa374e
Avielyo10 <Avielyo10@gmail.com>
2018-05-07 23:22:28 +0300

    minor fixes

--- Board.cpp
+++ Board.cpp
@@ -57,12 +57,14 @@ Board& Board::operator=(char c){
     return *this;
 }
 Board& Board::operator=(Board &b){
-    for(int i = 0; i < b.size;++i){
-        for(int j = 0; j < b.size;++j){
+    size=b.size;
+    board = new square*[size];
+    for(int i = 0; i < size;++i){
+        board[i] = new square[size];
+        for(int j = 0; j < size;++j){
             board[i][j] = b.board[i][j];
         }
     }
-    size=b.size;
     return *this;
 }
 bool Board::operator==(Board &b)const{
